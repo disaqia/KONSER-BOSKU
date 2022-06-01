7e07827b587a3107211d996cf9a72598e36fab1d
disaqia <106604515+disaqia@users.noreply.github.com>
2022-06-01 22:30:22 +0700

    copy paste

--- main.c
+++ main.c
@@ -1,9 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+int kesempatan=2;
+char nama[50],user[50], pass[50],adminU[20],adminP[20],cariA[30],cariB[30];
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//MENU MENU DALAM PROGRAM
+void registrasi();
+void login_user();
+void masuk_admin();
 
-int main(int argc, char *argv[]) {
-	
-	return 0;
+int menuawal;
+void main () {
+	puts("\t\t\t\t+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+	puts ("\t\t\t\tSELAMAT DATANG DI PROGRAM PEMESANAN KONSER MUSIK (D CONCERT)");
+	puts("\t\t\t\t+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+	puts ("Menu D Concert :");
+	puts ("1. User");
+	puts ("2. Admin");
+	puts ("3. Exit");
+	printf ("\nPilihan	:");
+	scanf("%d",&menuawal);
+	getchar();
+	system ("cls");
+	switch (menuawal) {
+		case 1:
+			registrasi();
+			break;
+		case 2:
+			masuk_admin();
+			break;
+		case 3:
+			system("cls");
+			break;
+		default :
+			printf("ERROR!!!\nTidak ada menu yang kamu pilih.");
+			break;
+	}
+}
+void registrasi () {
+
+	puts("\t\t\t\t\t++++++++++++++++++++++++++++++++++++++");
+	puts("\t\t\t\t\t\t  REGISTRASI AKUN ");
+	puts("\t\t\t\t\t++++++++++++++++++++++++++++++++++++++");
+	printf("\nNama Kamu\t : ");
+	gets(nama);
+	printf ("Username\t : ");
+	gets(user);
+	printf ("Password\t : ");
+	gets(pass);
+	printf("\nRegistrasi Sukses!");
+	getchar();
+	system("cls");
+	kesempatan=3;
+	login_user();
+}
+
+void login_user() {
+	puts("\t\t\t\t\t========================================");
+	puts("\t\t\t\t\t\t    LOGIN AKUN USER");
+	puts("\t\t\t\t\t========================================");
+	printf("Kesempatan login kamu %d kali",kesempatan);
+	printf ("\nUsername\t : ");
+	gets(cariA);
+	printf ("Password\t : ");
+	gets(cariB);
+	if (kesempatan>0) {
+		if (strcmp(user,cariA)==0&&strcmp(pass,cariB)==0) {
+			printf("\nLogin sukses\n");
+			printf("Lanjutkan");
+			getchar();
+		}
+		if (strcmp(user,cariA)!= 0 || strcmp(pass,cariB)!= 0) {
+			system("cls");
+			printf("Login Gagal\n");
+			kesempatan--;
+			login_user();
+			printf("Kesempatan login habis. Silahkan registrasi akun ulang!");
+			getchar();
+			system("cls");
+			main();
+		}
+	}
+}
+
+
+//MENU MENU ADMIN
+
+
+void masuk_admin() {
+
+	puts("\t\t\t\t\t================================");
+	puts ("\t\t\t\t\t\t  LOGIN ADMIN");
+	puts("\t\t\t\t\t================================");
+	printf ("\nUsername\t : ");
+	gets(adminU);
+	printf ("Password\t : ");
+	gets(adminP);
+	if (strcmp(adminU,"admin")==0&&strcmp(adminP,"admin")==0) {
+		printf("\nLogin sukses");
+		getchar();
+		system("cls");
+	} else {
+		printf("\nlogin gagal");
+		getchar();
+		system("cls");
+		main();
+	}
+
 }
