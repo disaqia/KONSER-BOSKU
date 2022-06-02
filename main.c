f4e1e84c8df04804e5d724b09727e1144a1e18e4
disaqia <106604515+disaqia@users.noreply.github.com>
2022-06-02 19:10:54 +0700

    new

--- main.c
+++ main.c
@@ -7,12 +7,16 @@
 #include "Admin.h"
 
 int kesempatan=2;
-char user[50], pass[50],adminU[20],adminP[20],cariA[30],cariB[30];
-
+char user[50], pass[50],adminU[20],adminP[20],cariA[30],cariB[30],cari[50],pilihanadmin[5];
+struct type {
+	char email[32], name[32], username[32], password[32], phone[23];
+	long int balance;
+} UserType;
 //MENU MENU DALAM PROGRAM
 void registrasi();
 void login_user();
 void masuk_admin();
+void searching_user();
 
 int menuawal;
 void main () {
@@ -129,4 +133,77 @@ void masuk_admin() {
 		return;
 	}
 
+}
+
+int menuAdmin;
+void MainAdmin()
+{
+	system("cls");
+	puts("\t\t\t\t+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+	puts ("\t\t\t\t\t\t DASHBOARD MENU ADMIN (D CONCERT)");
+	puts("\t\t\t\t+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+	puts ("Menu D Concert :");
+	puts ("1. Input Konser");
+	puts ("2. List Konser");
+	puts ("3. Tambah Konser");
+	puts ("4. Update Seat");
+	puts ("5. Searching User");
+	puts ("6. Logout");
+	printf ("\nPilihan	:");
+	scanf("%d",&menuAdmin);
+	getchar();
+	system ("cls");
+	switch (menuAdmin) {
+//		case 1:
+//			registrasi();
+//			break;
+		case 5:
+		searching_user();
+		break;
+		case 6:
+			return;
+			system("cls");
+			break;
+		default :
+			printf("ERROR!!!\nTidak ada menu yang kamu pilih.");
+			break;
+	}
+}
+
+void searching_user(){
+	system("cls");
+	
+	FILE*akun;
+ 	printf("------LIST AKUN USER-----\n\n");
+    int i = 1;
+    akun = fopen("db_akun.dat", "rb");
+    while (fread(&UserType, sizeof(UserType), 1, akun) == 1){
+        printf("%d.\tEmail\t: %s\n", i, UserType.email);
+        printf("\tNama\t: %s\n", UserType.name);
+        printf("\tUsername\t: %s\n", UserType.username);
+        printf("\tPassword: %s\n",UserType.password);
+        printf("\tNo HP: %s\n",UserType.phone);
+        UserType.balance = 0;
+        i++;
+    }
+    
+    printf("\nUsername yang dicari: ");
+    scanf("%s", &cari);
+    
+    if(UserType.username==cari){
+    printf("Akun ditemukan");
+}
+    else{
+	printf("akun tidak ditemukan\n");
+}
+	fclose(akun);
+	
+	printf("\napakah ingin kembali ke menu utama?(y/t) :"); fflush(stdin); gets(pilihanadmin);
+    if(strcmp(pilihanadmin,"y") ==0){
+     return main();
+     system("cls");
+     
+ }	else{
+  	printf("logout berhasil");
+ }
 }
